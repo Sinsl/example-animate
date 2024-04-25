ce0ba5475fcb795d8dfe1b322c31bebc238ea61c
Sinsl <sinsl@me.com>
2024-04-25 09:14:06 +0300

    change

--- js/main.js
+++ js/main.js
@@ -3,10 +3,12 @@ const navBox = document.querySelector('.nav-box')
 const about = document.querySelector('.about')
 const round1 = document.querySelector('.round-1')
 const leftBox = document.querySelector('.sl-left-box')
+let headerTopOffset = 0
 
 document.addEventListener('scroll', () => {
-    const {top} = header.getBoundingClientRect()
-    if (top < -200) {
+    headerTopOffset = header.getBoundingClientRect().top
+    // console.log('headerTop', top)
+    if (headerTopOffset < -200) {
         navBox.classList.add('scrolled')
     } else {
         navBox.classList.remove('scrolled')
@@ -24,17 +26,55 @@ document.addEventListener('scroll', () => {
     // if (topAbout < start) {
     //     round1.style.transform = `scale(${((start / topAbout - 1) * speed) < 1 ? 1 : ((start / topAbout - 1) * speed)})`
     // }
-
+    
 
 })
+
+const aboutContent = document.querySelector('.about-content')
+let isScrollAbout = false
+aboutContent.addEventListener('scroll', () => {
+    const {top: aboutTop} = about.getBoundingClientRect()
+    if (aboutTop > 5 || aboutTop < 5) {
+        isScrollAbout = true
+    }
+    if (isScrollAbout) {
+        window.scrollTo({
+            top: aboutTop + headerTopOffset * -1,
+            left: 0,
+            behavior: "smooth"
+          })
+          isScrollAbout = false
+    }
+})
+
+
+
+
+
+
 const roundContent = document.querySelector('.round-content')
 const boxRound = document.querySelector('.box-round')
+const rounded = document.querySelector('.rounded')
+let isScrollRound = false
 // console.log(boxRound)
 roundContent.addEventListener('scroll', () => {
+    const roundedTop = rounded.getBoundingClientRect().top
+    if (roundedTop > 5 || roundedTop < 5) {
+        isScrollRound = true
+    }
+    if (isScrollRound) {
+        window.scrollTo({
+            top: roundedTop + headerTopOffset * -1,
+            left: 0,
+            behavior: "smooth"
+          })
+          isScrollRound = false
+    }
+
     // console.log(roundContent.scrollTop)
     // roundContent.scrollTop = roundContent.scrollTop > 1500 ? 1500 : roundContent.scrollTop
-    boxRound.style.transform = `rotate(${roundContent.scrollTop * 0.18}deg)`;
-    if (roundContent.scrollTop > 1500) return
+    boxRound.style.transform = `rotate(${roundContent.scrollTop * 0.138}deg)`;
+    if (roundContent.scrollTop > 1950) return
 })
 
 VANTA.DOTS({
